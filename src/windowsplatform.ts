77e1626a871b5eaf5734e75b12503eea38248e54
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-11-04 09:18:55 +0100

    .

--- src/windowsplatform.ts
+++ src/windowsplatform.ts
@@ -45,6 +45,7 @@ export class MsvcPlatform extends WindowsPlatform
 {
     public addExtraEnvVars(basePath: string): void {
         core.exportVariable("VSINSTALLDIR", "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise")
+        core.exportVariable("VSCMD_DEBUG", "3");
     }
 
     public async runInstaller(tool: string, args: string[], instDir: string): Promise<void> {
