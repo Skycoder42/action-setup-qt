90492203b98931ba03c2cdebb4c93477fdfecaad
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-11-04 10:45:13 +0100

    install missing linux deps

--- src/linuxplatform.ts
+++ src/linuxplatform.ts
@@ -15,13 +15,14 @@ export class LinuxPlatform extends UnixPlatform
     }
 
     public async runInstaller(tool: string, args: string[], instDir: string): Promise<void> {
+        await ex.exec("sudo", ["apt-get", "-qq", "install", "libgl1-mesa-dev"]);
 		await fs.chmod(tool, 0o755);
 		const options: any = {};
 		options.env = {
 			"QT_QPA_PLATFORM": "minimal",
 			"HOME": path.join(instDir, "..", 'home')
 		};
-		await ex.exec(tool, args, options);
+        await ex.exec(tool, args, options);
     }
 }
 
