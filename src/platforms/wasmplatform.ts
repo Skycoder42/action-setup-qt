9837067ae66616bcbd47e2e2ef41842822d6d03b
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-06-25 18:06:40 +0200

    fix wasm paths

--- src/platforms/wasmplatform.ts
+++ src/platforms/wasmplatform.ts
@@ -1,3 +1,5 @@
+import { join } from "path";
+
 import LinuxPlatform from "./linuxplatform";
 import { CMakeConfigMap } from "./platform";
 
@@ -5,10 +7,10 @@ export default class WasmPlatform extends LinuxPlatform {
   public cmakeArgs(): CMakeConfigMap {
     // https://github.com/forderud/QtWasm/blob/master/source/wasm.cmake
     return {
-      CMAKE_TOOLCHAIN_FILE:
-        "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake",
-      CMAKE_EXECUTABLE_SUFFIX: ".wasm.js",
-      CMAKE_EXE_LINKER_FLAGS: "-s WASM=1 -s EXPORTED_FUNCTIONS='[_main]",
+      CMAKE_TOOLCHAIN_FILE: join(
+        process.env["EMSDK"]!,
+        "upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+      ),
     };
   }
 }
